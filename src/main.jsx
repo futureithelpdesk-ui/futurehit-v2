779696aacc411935d912c94b9768305116fc159d
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-10 17:57:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { getRouter } from './router'
+import App from './App'
 import './styles.css'
 
-const queryClient = new QueryClient()
-const router = getRouter()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 )
