b6b892ffc5e4e88af580b7e0914e56e5080fdbfc
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-10 22:53:36 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -13,7 +13,7 @@ self.addEventListener('message', (event) => {
     if (event.data?.type === 'SHOW_NOTIFICATION') {
         self.registration.showNotification(event.data.title, {
             body: event.data.body,
-            icon: event.data.icon || '/favicon.ico',
+            icon: '/favicon.ico',
             badge: '/favicon.ico',
             vibrate: [200, 100, 200]
         });
