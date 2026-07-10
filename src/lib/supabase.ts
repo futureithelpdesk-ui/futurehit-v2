551f165e29373cc2039124598daf43e47a9da90b
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-10 17:25:49 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://wrsctddishgwflfbnfcx.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indyc2N0ZGRpc2hnd2ZsZmJuZmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1OTY0NTgsImV4cCI6MjA5OTE3MjQ1OH0.-r7Aha9CiPNC-r4ztqNnN3JVsQTdbupBnSuUPW_TKpg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
