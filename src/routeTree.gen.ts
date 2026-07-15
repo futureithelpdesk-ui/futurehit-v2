8d78583ae12bb2f69b0578efb838d395c83f7c8c
futureithelpdesk-ui <futureithelpdesk@gmail.com>
2026-07-16 00:09:28 +0530

    Update routeTree.gen.ts

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,13 +1,13 @@
+import { createRootRoute, createRoute, createRouter } from '@tanstack/react-router'
 import { Route as rootRoute } from './routes/__root'
 import { Route as IndexRoute } from './routes/index'
 import { Route as AuthRoute } from './routes/auth'
 import { Route as CommandRoute } from './routes/command'
 
-const rootRouteChildren = {
+const routeTree = rootRoute.addChildren([
   IndexRoute,
   AuthRoute,
   CommandRoute,
-}
+])
 
-export const routeTree = rootRoute
-  ._addFileChildren(rootRouteChildren)
+export { routeTree }
